8a310c40da00a27c8bb0ed789b49c2373d48ed41
Anuraj-65 <anuraaj65@gmail.com>
2026-01-18 01:41:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "https://tibetankeys.com", // Your Firebase domain
+    methods: ["GET", "POST"]
+  }
+});
 const path = require('path');
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 3000;
 const GAME_DURATION = 60; 
 
@@ -169,6 +175,5 @@ function endGame(room) {
 }
 
 function getLobbyData() { return rooms.map(r => ({ id: r.id, count: r.players.length, status: r.status, timeLeft: r.timer })); }
-
 
 http.listen(PORT, () => { console.log(`Server running on http://localhost:${PORT}`); });
